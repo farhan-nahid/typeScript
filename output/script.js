d1eeda4089682dc0d5c4efa9175290fb0130faad
farhan-nahid <nahidahmed9933@gmail.com>
2021-09-12 15:15:01 +0600

    part 2 compleate

--- output/script.js
+++ output/script.js
@@ -1,3 +1,5 @@
 "use strict";
 var a = 2;
 console.log(a);
+var myName = "Farhan";
+console.log(myName);
--- src/script.ts
+++ src/script.ts
@@ -1,2 +1,5 @@
 const a = 2;
 console.log(a);
+
+const myName = "Farhan";
+console.log(myName);
