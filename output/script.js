5483b9a530a1d56f135ca9beaf879092888b2716
farhan-nahid <nahidahmed9933@gmail.com>
2021-09-12 15:51:13 +0600

    type checking

--- output/script.js
+++ output/script.js
@@ -3,3 +3,42 @@ var a = 2;
 console.log(a);
 var myName = "Farhan";
 console.log(myName);
+/*
+
+                string 🍔🌭🍗
+
+*/
+var country = "Bangladesh";
+// if i assign a number type value it's will give us en error
+country = "Bhutan";
+console.log(country);
+/*
+
+                number 🍔🌭🍗
+
+*/
+var age = 20;
+// if i assign a string type value it's will give us en error
+age = 21;
+console.log(age);
+/*
+
+                array 🍔🌭🍗
+
+*/
+var myArray = [20, 30, 40, false, 9, 8, true];
+// we can't push any string or object in this array
+myArray.push(6);
+myArray.push(false);
+/*
+
+                object 🍔🌭🍗
+
+*/
+var player = {
+    name: "Mashrafi",
+    isCaptain: true,
+    age: 35,
+};
+player.name = "Shakib";
+// but we cannot give hare any property which is not in object
--- src/script.ts
+++ src/script.ts
@@ -3,3 +3,54 @@ console.log(a);
 
 const myName = "Farhan";
 console.log(myName);
+
+/* 
+
+                string 🍔🌭🍗
+
+*/
+
+let country = "Bangladesh";
+
+// if i assign a number type value it's will give us en error
+country = "Bhutan";
+console.log(country);
+
+/* 
+
+                number 🍔🌭🍗
+
+*/
+
+let age: number = 20;
+
+// if i assign a string type value it's will give us en error
+age = 21;
+console.log(age);
+
+/* 
+
+                array 🍔🌭🍗
+
+*/
+
+const myArray = [20, 30, 40, false, 9, 8, true];
+
+// we can't push any string or object in this array
+myArray.push(6);
+myArray.push(false);
+
+/* 
+
+                object 🍔🌭🍗
+
+*/
+
+const player = {
+  name: "Mashrafi",
+  isCaptain: true,
+  age: 35,
+};
+
+player.name = "Shakib";
+// but we cannot give hare any property which is not in object
